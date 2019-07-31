c9180db7eaf325d93ae9859a6e9f2aaaddf3132a
Geor Kasapidi <geor.kasapidi@icloud.com>
2019-07-31 23:17:19 +0300

    FLGO-109 use raw sql for user operations

--- app/db/work.go
+++ app/db/work.go
@@ -1,21 +1,7 @@
 package db
 
-import "fantlab/utils"
-
 func (db *DB) WorkExists(workId uint64) error {
-	const workExistsQuery = "SELECT EXISTS(SELECT 1 FROM works WHERE work_id = ?)"
-
 	var workExists bool
-
-	err := db.X.Get(&workExists, workExistsQuery, workId)
-
-	if err != nil {
-		return err
-	}
-
-	if !workExists {
-		return utils.ErrRecordNotFound
-	}
-
-	return nil
+	err := db.X.Get(&workExists, "SELECT 1 FROM works WHERE work_id = ?", workId)
+	return err
 }
