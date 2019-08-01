f0441b0c5ba06df62212aa9817dd207e650c465e
Geor Kasapidi <geor.kasapidi@icloud.com>
2019-08-01 14:19:07 +0300

    FLGO-119 check user in middlewares

--- app/middlewares/session.go
+++ app/middlewares/session.go
@@ -1,13 +1,15 @@
 package middlewares
 
 import (
+	"fantlab/pb"
 	"fantlab/shared"
 	"fantlab/utils"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Session(services *shared.Services) gin.HandlerFunc {
+func DetectUser(services *shared.Services) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		sid := ctx.GetHeader(utils.SessionHeader)
 
@@ -31,6 +33,34 @@ func Session(services *shared.Services) gin.HandlerFunc {
 			ctx.Set(gin.AuthUserKey, int64(0))
 		}
 
+		ctx.Next()
+	}
+}
+
+func AuthorizedUserIsRequired(ctx *gin.Context) {
+	userId := ctx.GetInt64(gin.AuthUserKey)
+
+	if userId > 0 {
+		ctx.Next()
+	} else {
+		utils.ShowProto(ctx, http.StatusUnauthorized, &pb.Error_Response{
+			Status: pb.Error_INVALID_SESSION,
+		})
+
+		ctx.Abort()
+	}
+}
+
+func AnonymousIsRequired(ctx *gin.Context) {
+	userId := ctx.GetInt64(gin.AuthUserKey)
+
+	if userId > 0 {
+		utils.ShowProto(ctx, http.StatusMethodNotAllowed, &pb.Error_Response{
+			Status: pb.Error_LOG_OUT_FIRST,
+		})
+
+		ctx.Abort()
+	} else {
 		ctx.Next()
 	}
 }
