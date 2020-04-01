45a340b1a238f346fe37a584bba51f30cece5e31
Geor Kasapidi <geor.kasapidi@icloud.com>
2020-04-02 00:27:32 +0300

    FLGO-18 fix panic

--- sources/server/internal/app/redis.go
+++ sources/server/internal/app/redis.go
@@ -9,10 +9,10 @@ import (
 )
 
 func (s *Services) BlogTopicsViewCount(ctx context.Context, topicIds []uint64) []uint64 {
-	if s.redis == nil {
-		return nil
-	}
 	result := make([]uint64, len(topicIds))
+	if s.redis == nil {
+		return result
+	}
 	_ = s.redis.Perform(ctx, func(conn redisclient.Conn) error {
 		for index, topicId := range topicIds {
 			viewCount, _ := redis.Uint64(conn.Do("PFCOUNT", fmt.Sprintf("blogtopicviews:%d", topicId)))
