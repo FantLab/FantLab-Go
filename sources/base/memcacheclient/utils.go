e4e08e689cc0edeb4f13c89521b49df3fcb82f64
Geor Kasapidi <geor.kasapidi@icloud.com>
2020-04-30 12:59:54 +0300

    improve memcache usage

--- sources/base/memcacheclient/utils.go
+++ sources/base/memcacheclient/utils.go
@@ -1,7 +1,20 @@
 package memcacheclient
 
-import "github.com/bradfitz/gomemcache/memcache"
+import (
+	"github.com/bradfitz/gomemcache/memcache"
+)
 
 func IsNotFoundError(err error) bool {
 	return err == memcache.ErrCacheMiss
+}
+
+func Perform(client Client, ignoreNotFoundErrors bool, action func(Client) error) error {
+	if client == nil || action == nil {
+		return nil
+	}
+	err := action(client)
+	if ignoreNotFoundErrors && IsNotFoundError(err) {
+		return nil
+	}
+	return err
 }
